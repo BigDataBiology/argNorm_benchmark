00e914a1804f39e4cbcf75f67e810099fa64b128
Vedanth <vedanth.ramji@outlook.com>
2024-01-15 21:31:40 +0530

    initial commit

--- data/scripts.py
+++ data/scripts.py
@@ -10,6 +10,7 @@ def create_csv_files(output_file_name, field_names, data):
 
 def get_count_not_mapped():
     data = []
+    not_mapped_list = []
     curation_root = 'C:/Users/vedan/BDB-FUDAN/argNorm_benchmark/data/manual_curation/'
     manual_curation_files = os.listdir(curation_root)
 
@@ -18,15 +19,23 @@ def get_count_not_mapped():
         count = 0
 
         for i in range(manual_curation_file.shape[0]):
-            if not manual_curation_file.loc[i, 'ARO Replacement'].isnumeric():
+            if not manual_curation_file.loc[i, 'ARO Replacement'].isnumeric() and not "{'ARO:" in manual_curation_file.loc[i, 'ARO Replacement']:
                 count += 1
+                not_mapped_list.append([manual_curation_file.loc[i, 'ARO Replacement'], i])
                 print(manual_curation_file.loc[i, 'ARO Replacement'])
 
-        data.append({
-            'File': file,
-            'Num Not Mapped': count,
-        })
+        for i in not_mapped_list:
+            data.append({
+                'File': file,
+                'Mapping': i[0],
+                'Position': i[1]
+            })
 
-    create_csv_files('num_not_mapped.csv', ['File', 'Num Not Mapped'], data)
+        # data.append({
+        #     'File': file,
+        #     'Num Not Mapped': count,
+        # })
+
+    create_csv_files('not_mapped_mapping.csv', ['File', 'Mapping', 'Position'], data)
 
 get_count_not_mapped()
